b84957b1346ead9f880a8c742601acf52e021deb
Rey Narvaez <reynaldo_narvaez@hotmail.com>
2024-02-13 19:20:59 -0600

    Implement dashboard page

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music,
+  Settings,
+  VideoIcon,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
@@ -13,51 +21,50 @@ const montserrat = Montserrat({
 });
 
 const routes = [
-    {
-        label: "Dashboard",
-        icon: LayoutDashboard,
-        href: "/dashboard",
-        color: "text-sky-500",
-    },
-    {
-        label: "Conversation",
-        icon: MessageSquare,
-        href: "/conversation",
-        color: "text-violet-500",
-    },
-    {
-        label: "Image Generation",
-        icon: ImageIcon,
-        href: "/image",
-        color: "text-pink-700",
-    },
-    {
-        label: "Video Generation",
-        icon: VideoIcon,
-        href: "/video",
-        color: "text-orange-700",
-    },
-    {
-        label: "Music Generation",
-        icon: Music,
-        href: "/music",
-        color: "text-emerald-500",
-    },
-    {
-        label: "Code Generation",
-        icon: Code,
-        href: "/code",
-        color: "text-green-700",
-    },
-    {
-        label: "Settings",
-        icon: Settings,
-        href: "/settings",
-    },
+  {
+    color: "text-sky-500",
+    href: "/dashboard",
+    icon: LayoutDashboard,
+    label: "Dashboard",
+  },
+  {
+    color: "text-violet-500",
+    href: "/conversation",
+    icon: MessageSquare,
+    label: "Conversation",
+  },
+  {
+    color: "text-pink-700",
+    href: "/image",
+    icon: ImageIcon,
+    label: "Image Generation",
+  },
+  {
+    color: "text-orange-700",
+    href: "/video",
+    icon: VideoIcon,
+    label: "Video Generation",
+  },
+  {
+    color: "text-emerald-500",
+    href: "/music",
+    icon: Music,
+    label: "Music Generation",
+  },
+  {
+    color: "text-green-700",
+    href: "/code",
+    icon: Code,
+    label: "Code Generation",
+  },
+  {
+    href: "/settings",
+    icon: Settings,
+    label: "Settings",
+  },
 ];
 
 const Sidebar = () => {
-
   const pathname = usePathname();
 
   return (
@@ -72,19 +79,23 @@ const Sidebar = () => {
           </h1>
         </Link>
         <div className="space-y-1">
-            {routes.map((route) => (
-                <Link 
-                    href={route.href}
-                    key={route.href}
-                    className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                      pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
-                    )}>
-                        <div className="flex items-center flex-1">
-                            <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
-                            {route.label}
-                        </div>
-                </Link>
-            ))}
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
