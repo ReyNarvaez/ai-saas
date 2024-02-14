2ec8c092561c64a49c5a67f2125091c89bcf722d
Rey Narvaez <reynaldo_narvaez@hotmail.com>
2024-02-13 18:34:29 -0600

    Implement sidebar

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">Explore the power of AI</h2>
+        <p>Chat with the smartest AI - Experience the power of AI</p>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
